10bbf2c44286c8a9735bdb1a1578839a1e7b8502
mrsalvini <dsgmcosta@gmail.com>
2019-08-13 22:22:50 +0100

    redux functionality under way

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import React, { Fragment, useEffect } from 'react'
 import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css/dist/js/materialize.min.js'
 import './App.css'
-import SearchBar from './components/layout/SearchBar';
-import Logs from './components/logs/Logs';
-import AddBtn from './components/layout/AddBtn';
-import AddLogModal from './components/logs/AddLogModal';
-import EditLogModal from './components/logs/EditLogModal';
-import AddTechModal from './components/techs/AddTechModal';
-import TechListModal from './components/techs/TechListModal';
+import SearchBar from './components/layout/SearchBar'
+import Logs from './components/logs/Logs'
+import AddBtn from './components/layout/AddBtn'
+import AddLogModal from './components/logs/AddLogModal'
+import EditLogModal from './components/logs/EditLogModal'
+import AddTechModal from './components/techs/AddTechModal'
+import TechListModal from './components/techs/TechListModal'
+import { Provider } from 'react-redux'
+import store from './store'
+
 
 const App = () => {
   useEffect(() => {
@@ -18,18 +21,20 @@ const App = () => {
   })
 
   return (
-    <Fragment>
-
-      <SearchBar />
-      <div className="container">
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </Fragment>)
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
+  )
 }
 
 export default App
